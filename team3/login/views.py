a3d3b92c6975fc12e83f7af4697e81610b7597b1
LukeHarrold <lvh3851@g.rit.edu>
2015-10-17 07:58:42 -0400

    added some kinda functionality

--- team3/login/views.py
+++ team3/login/views.py
@@ -1,46 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.template import RequestContext, loader
-
+from login.forms import NavBarForm, RegisterForm
 import datetime
 
 
 def user_login(request):
 	#username = request.POST.get('username', False)
 	#password = request.POST.get('password', False)
-	form = NavBarForm(POST.request)
-	username = form.username
-	password = form.password
-	user = authenticate(username=username, password=password)
-	good_template = loader.get_template('user_login.html')
-	exp_template = loader.get_template('exp_user.html')
-	bad_template = loader.get_template('bad_login.html')
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			# Redirect to a success page.'
-			return HttpResponse(good_template.render())
+	if request.method == 'POST':
+		form = NavBarForm(request.POST)
+		username = form.clean_data['username']
+		password = form.clean_data['password']
+		user = authenticate(username=username, password=password)
+		if (user is not None) and form.is_valid():
+			if user.is_active:
+				login(request, user)
+				# Redirect to a success page.'
+				return HttpResponseRedirect('user_login.html')
+			else:
+				
+				return HttpResponseRedirect('exp_user/')
+	            
 		else:
 			
-			return HttpResponse(exp_template.render())
-            
+			
+			return HttpResponseRedirect('exp_user.html')
+			#return render(request, request.META.get('HTTP_REFERER'), {'form':form})
 	else:
-		
-		return HttpResponse(bad_template.render())
-        # Return an 'invalid login' error message.
-        
-        
-def register_user(requests):
-	username = request.POST.get('username', False)
-	password = request.POST.get('password', False)
-	email = request.POST.get('email', False)
-	user = User.object.create_user(username, email, password)
 
-	#user.stuff = 'new stuff'
-	user.save()
-	good_template = loader.get_template('user_login.html')
-	return HttpResponse(good_template.render())
+	#	form = NavBarForm()
+		return HttpResponseRedirect( 'user_login.html')
+
+def register_user(requests):
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		username = form.clean_data['username']
+		password = form.clean_data['password']
+		email = form.clean_data['email']
+		user = User.object.create_user(username, email, password)
+
+		#user.stuff = 'new stuff'
+		user.save()
+		good_template = loader.get_template('user_login.html')
+		return HttpResponseRedirect('good_register.html')
+	else:
+		form = RegisterForm()
+		return render(request, 'endmysuffering', {'form':form})
 
 def registration_page(request):
-	return render(request, 'registration.html')
+	return render(request, 'registration.html' )
