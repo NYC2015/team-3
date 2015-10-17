1664fae600a369de8cf7d02da1699d9f0cb8427d
Christina <cc3561@nyu.edu>
2015-10-17 02:30:02 -0400

    map search works

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function searchmap(){
-    var place = document.getElementById("searchloc");
+function newloc(){
+    var place = document.getElementById("searchloc").value;
     var themap = document.getElementById("googlemap");
     themap.src="https://maps.google.com/maps?hl=en&q=" + place + "&ie=UTF8&t=roadmap&z=6&iwloc=B&output=embed";
+    console.log(themap.src);
 }
