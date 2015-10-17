c5bc1b5472cad7f52e9c04cc89712e029a508422
Kong Huang <wheressswaldo@gmail.com>
2015-10-16 22:28:34 -0400

    luke fix community

--- team3/community/models.py
+++ team3/community/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Community(models.Model):
+	users
+	description = models.TextField()
+	posts = models.ManyToManyField("self")
+	events
+
+class Post(models.Model):
+	author = models.ForeignKey(User)
+	created_on = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+	
+	def __unicode__(self):
+		return self.text
+
+
+class Comment(models.Model):
+	author = models.ForeignKey(User)
+	post = models.ForeignKey(Post)
+	created_on = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+	
+	def __unicode__(self):
+		return self.text
+	
+class Event(models.Model):
+	author = models.ForeignKey(User)
+	created_on = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+	location = models.CharField(max_length=200)
+	guests = models.ManyToManyField("self")
+	
+	@property
+	def guestlist(self):
+		# watch for large querysets!
+		return list(self.guests.all())
