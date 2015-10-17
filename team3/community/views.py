7038b774077180cf45e5c55eafee786d78a34395
Derek Leung <dxl3349@rit.edu>
2015-10-17 00:53:33 -0400

    routed urls. still need to route /ask and /registration

--- team3/community/views.py
+++ team3/community/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'community/index.html')
+
+def create(request):
+	return render(request, 'community/makecommunities.html')
